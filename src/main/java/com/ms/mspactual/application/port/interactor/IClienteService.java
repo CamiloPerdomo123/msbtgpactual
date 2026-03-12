2cf3fd0eb27fd1320c3a166d3b359fda05de1dd6
Juan Camilo Perdomo Carreno <jperdomo.externo@coopeuch.cl>
2026-03-12 15:12:13 -0500

    first commit.

--- src/main/java/com/ms/mspactual/application/port/interactor/IClienteService.java
+++ src/main/java/com/ms/mspactual/application/port/interactor/IClienteService.java
@@ -2,7 +2,6 @@ package com.ms.mspactual.application.port.interactor;
 
 import com.ms.mspactual.application.dto.in.cliente.ClienteRequestDto;
 import com.ms.mspactual.application.dto.on.cliente.ClienteResponse;
-import com.ms.mspactual.infrastructure.repository.entity.Cliente;
 
 public interface IClienteService {
     ClienteResponse crearCliente(ClienteRequestDto cliente);
